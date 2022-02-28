fac448964fef70ba5b315277e2ae247726151a6b
Nahum Santana <nahumsantanam@gmail.com>
2022-02-28 18:55:07 -0400

    feat(frontend): added user account preview in header

--- vue_frontend/src/components/budget.vue
+++ vue_frontend/src/components/budget.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="w-full bg-primary rounded px-4 py-6 flex flex-col gap-2 h-max">
+    <div class="w-full bg-primary rounded-1 px-4 py-6 flex flex-col gap-2 h-max">
         <h1>PRESUPUESTO</h1>
         <div class="w-full border border-secondary mb-4"></div>
-        <div class="rounded flex gap-2 border border-primary px-1 py-1">
+        <div class="rounded-1 flex gap-2 border border-primary px-1 py-1">
             <input class="w-full bg-primary border-none" type="text" v-model="budgetTitle" placeholder="Inserte un título...">
             <div class="border border-primary"></div>
             <input class="w-full bg-primary border-none" type="number" v-model="budgetAmount">
@@ -10,7 +10,7 @@
                 <img width="20" height="20" src="/src/assets/icons/add-circle.svg">
             </button>
         </div>
-        <div class="rounded flex gap-2 border border-primary px-1 py-1 show-on" v-for="(budget, index) in budgets" :key="budget">
+        <div class="rounded-1 flex gap-2 border border-primary px-1 py-1 show-on" v-for="(budget, index) in budgets" :key="budget">
             <input class="w-full bg-primary border-none" type="text" v-model="budget.title">
             <div class="border border-primary"></div>
             <input class="w-full bg-primary border-none" type="number" v-model="budget.amount">
--- vue_frontend/src/components/summary.vue
+++ vue_frontend/src/components/summary.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="w-1d2 h-max bg-primary rounded px-4 py-6 flex flex-col gap-2">
+    <div class="w-1d2 h-max bg-primary rounded-1 px-4 py-6 flex flex-col gap-2">
         <h1>RESUMEN</h1>
         <div class="w-full border border-secondary mb-4"></div>
-        <div class="rounded flex justify-between border border-primary px-1 py-1">
+        <div class="rounded-1 flex justify-between border border-primary px-1 py-1">
             <p>Ingresos:</p>
             <p>$0</p>
         </div>
-        <div class="rounded flex justify-between border border-primary px-1 py-1">
+        <div class="rounded-1 flex justify-between border border-primary px-1 py-1">
             <p>Egressos:</p>
             <p>$0</p>
         </div>
         <div class="w-full border border-secondary mt-2 mb-2"></div>
         
-        <div class="rounded flex justify-between border border-primary px-1 py-1">
+        <div class="rounded-1 flex justify-between border border-primary px-1 py-1">
             <p>Total:</p>
             <p>$0</p>
         </div>
