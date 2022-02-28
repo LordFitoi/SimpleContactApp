078fe6f75cc03e85d405e848a1a2a1041d8cd894
Nahum Santana <nahumsantanam@gmail.com>
2022-02-28 19:15:11 -0400

    fix(frontend): layout color pallet fixed

--- vue_frontend/src/components/summary.vue
+++ vue_frontend/src/components/summary.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-1d2 h-max bg-primary rounded-1 px-4 py-6 flex flex-col gap-2">
-        <h1>RESUMEN</h1>
+        <h2 class="font-lg">RESUMEN</h2>
         <div class="w-full border border-secondary mb-4"></div>
         <div class="rounded-1 flex justify-between border border-primary px-1 py-1">
             <p>Ingresos:</p>
